15d831f66a8f2a907bf31b581b8ad79eab58c1fd
kushagra216 <72160516+kushagra216@users.noreply.github.com>
2020-10-01 18:53:13 +0530

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,23 +1,25 @@
 #include <iostream> 
+using namespace std;
 #include<stdlib.h>
 #include<time.h>
-using namespace std;
-int main(){
+
+int main()
+    {
       int number,guess,attempt=0;
       srand(time(0));
             number=rand()%100 + 1;
             do
             {
-                cout<<"\t\t\tGuess the number between 1 and 100\n";
+                cout<<"\t\t\tGuess the number between 1 and 100\n\n";
                 cin>>guess;
                 if (guess<number)
                 {
-                  cout<<"\t\t\tGuess a little higher\n";
+                  cout<<"\t\t\tGuess a little higher\n\n";
                   attempt++;
                 }
                 else if (guess>number)
                 {
-                  cout<<"\t\t\tGuess a bit lower\n"; 
+                  cout<<"\t\t\tGuess a bit lower\n\n"; 
                    attempt++;               }
                 else
                 {          
