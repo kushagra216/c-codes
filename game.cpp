71837c911f3e9b885cacac9a475fe963333ef0e7
kushagrasharma217 <73079668+kushagrasharma217@users.noreply.github.com>
2020-10-19 02:20:06 +0530

    Update game.cpp
    inlined the code 
    changed the header files pre processor directives 
    optimised the code 
    enhanced the readiablility

--- game.cpp
+++ game.cpp
@@ -1,8 +1,7 @@
-#include <iostream> 
+#include <iostream.h> 
 using namespace std;
-#include<stdlib.h>
+#include<stdlib>
 #include<time.h>
-
 int main()
     {
       int number,guess,attempt=0;
@@ -10,16 +9,16 @@ int main()
             number=rand()%100 + 1;
             do
             {
-                cout<<"\t\t\tGuess the number between 1 and 100\n\n";
+                cout<<"\t\t\tGuess the number between 1 and 100<<endl;
                 cin>>guess;
                 if (guess<number)
                 {
-                  cout<<"\t\t\tGuess a little higher\n\n";
+                  cout<<"\t\t\tGuess a little higher\n"<<endl;
                   attempt++;
                 }
                 else if (guess>number)
                 {
-                  cout<<"\t\t\tGuess a bit lower\n\n"; 
+                  cout<<"\t\t\tGuess a bit lower\n"<<endl; 
                    attempt++;               }
                 else
                 {          
@@ -27,8 +26,7 @@ int main()
                     }
                   
             } while (number!=guess);
-            
-        return 0;
+           return 0;
   }
 
 
